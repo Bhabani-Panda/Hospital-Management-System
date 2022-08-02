26fdaeec16b9ee213e7b8d8317e987ccae3dd1c6
Bhabani Shankar Panda <99830064+Bhabani-Panda@users.noreply.github.com>
2022-08-03 01:07:26 +0530

    Delete Other Directory

--- Lacuisine Restaurant PHP Project/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include ("connection/connect.php");
-session_start(); 				//start session
-session_destroy(); 				//distroy all the current sessions
-header("Location: index.php"); 	//redireted to login page
-?>
